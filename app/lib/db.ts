8782a87baa80b0c23ebe1d924e31e59ea33d4838
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-07-09 21:28:33 +0800

    Censor users' email (#32)
    * Require exact match for email
    
    * Remove email from search results

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -31,15 +31,9 @@ export async function getUsersFromNameOrEmail(
           contains: query,
         },
       };
-  const filterEmail = exact
-    ? {
-        email: query,
-      }
-    : {
-        email: {
-          contains: query,
-        },
-      };
+  const filterEmail = {
+    email: query,
+  };
   return await prisma.user.findMany({
     where: {
       OR: [filterName, filterEmail],
@@ -47,7 +41,6 @@ export async function getUsersFromNameOrEmail(
     select: {
       id: true,
       name: true,
-      email: true,
     },
     take: limit,
   });
