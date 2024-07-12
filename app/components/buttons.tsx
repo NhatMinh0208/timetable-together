24681af223c3061ce4d182eda33e98acb174ec0f
NhatMinh0208 <minhkhicon2468@gmail.com>
2024-07-12 23:08:02 +0700

    Fix styling for header

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -59,7 +59,7 @@ export function MyEvents({ labl }: { labl: string }) {
   return (
     <Link
       href={"/myevents"}
-      className="flex mx-auto justify-center rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      className="flex justify-center rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
     >
       {labl}
     </Link>
