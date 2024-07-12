a69339ed2412dd423574486ef1fe6af5939eb33e
keyansheng <65121402+keyansheng@users.noreply.github.com>
2024-07-12 14:09:33 +0800

    Remove header classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,37 +30,31 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={clsx(inter.className, "flex h-dvh flex-col")}>
-        <div className="w-dvw px-2 pt-2 pb-0 grow-0">
-          <div
-            className={
-              "h-full px-2 py-2 space-y-2 rounded-lg bg-slate-200 flex flex-row align-middle"
-            }
-          >
-            <Link
+        <div className="flex rounded-lg bg-slate-200">
+          <Link
               href="/"
               className="h-full space-y-2 rounded-lg bg-slate-200 flex flex-row align-middle"
             >
-              <Image src={Icon} alt="Logo" width={50} height={50}></Image>
+            <Image src={Icon} alt="Logo" width={50} height={50}></Image>
+            <span className="w-2"></span>
+            <span className="text-2xl">Timetable Together</span>
+          </Link>
+          <span className="grow"></span>
+          {session ? (
+            <>
+              <span className="text-2xl">{session?.user?.name}</span>
               <span className="w-2"></span>
-              <span className="text-2xl">Timetable Together</span>
-            </Link>
-            <span className="grow"></span>
-            {session ? (
-              <>
-                <span className="text-2xl">{session?.user?.name}</span>
-                <span className="w-2"></span>
-                <Timetable labl="Timetable" />
-                <span className="w-2"></span>
-                <Logout labl="Sign out" />
-              </>
-            ) : (
-              <>
-                <Login labl={"Sign in"} />
-                <span className="w-2"></span>
-                <Register labl={"Register"} />
-              </>
-            )}
-          </div>
+              <Timetable labl="Timetable" />
+              <span className="w-2"></span>
+              <Logout labl="Sign out" />
+            </>
+          ) : (
+            <>
+              <Login labl={"Sign in"} />
+              <span className="w-2"></span>
+              <Register labl={"Register"} />
+            </>
+          )}
         </div>
         <div className="w-dvw min-h-0 grow">{children}</div>
       </body>
